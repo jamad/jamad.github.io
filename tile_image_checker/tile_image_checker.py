e39c04dc5fcd85e65225292d8040d2c27fe8af42
jamad <jamad2001@gmail.com>
2023-07-24 16:00:19 +0300

    Update tile_image_checker.py

--- tile_image_checker/tile_image_checker.py
+++ tile_image_checker/tile_image_checker.py
@@ -1,2 +1,16 @@
+# app.py
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
 def greeting(name):
     return f"Hello, {name}! Welcome to GitHub Pages!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    message = greeting(name)
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    app.run()
