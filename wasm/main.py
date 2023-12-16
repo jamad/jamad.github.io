210a7ad0bcecc3d415990180ecc1b1a69433cbd8
jamad <jamad2001@gmail.com>
2023-12-16 21:04:15 +0200

    Update main.py

--- wasm/main.py
+++ wasm/main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from pygame.math import Vector2
 
+import asyncio
+
 BLUE = (0,0,32)
 DARK_BLUE = (0, 128, 0)
 CELL_SIZE = 2
@@ -14,33 +16,39 @@ screen = pygame.display.set_mode( [2*OFFSET + CELL_SIZE*CELL_COUNT]*2)
 SNAKE_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_UPDATE, 15) 	# the smaller, the faster
 
-init=True
-while True:
-	if init:
-		bodies=[Vector2(i, 9) for i in range(18,3,-1)]
-		dir = Vector2(1,0)
-		init=False
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit(pygame.quit())
-		if event.type == SNAKE_UPDATE:
-			new_head=bodies[0] + dir
-			bodies = [new_head]+bodies[:-1]	
-			if 	not -1<new_head.x <= CELL_COUNT or not -1<new_head.y <= CELL_COUNT or	new_head in bodies[1:]:	
-				init=True
-		if event.type == pygame.KEYDOWN:
-			dir =DIRECTION.get(event.key,dir) # default : no change
 
-	#Draw
-	screen.fill(BLUE)
-	pygame.draw.rect(screen, DARK_BLUE, (OFFSET-5, OFFSET-5, CELL_SIZE*CELL_COUNT+2, CELL_SIZE*CELL_COUNT+2), 5)
-	
-	for body in bodies:
-		body_rect = (OFFSET + body.x * CELL_SIZE, OFFSET+ body.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-		pygame.draw.rect(screen, DARK_BLUE, body_rect, 0, 7)
+async def main():
+	init=True
+	while True:
+		if init:
+			bodies=[Vector2(i, 9) for i in range(18,3,-1)]
+			dir = Vector2(1,0)
+			init=False
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit(pygame.quit())
+			if event.type == SNAKE_UPDATE:
+				new_head=bodies[0] + dir
+				bodies = [new_head]+bodies[:-1]	
+				if 	not -1<new_head.x <= CELL_COUNT or not -1<new_head.y <= CELL_COUNT or	new_head in bodies[1:]:	
+					init=True
+			if event.type == pygame.KEYDOWN:
+				dir =DIRECTION.get(event.key,dir) # default : no change
 
-	title_surface = pygame.font.Font(None, 15).render("Simple Snake", True, DARK_BLUE)
+		#Draw
+		screen.fill(BLUE)
+		pygame.draw.rect(screen, DARK_BLUE, (OFFSET-5, OFFSET-5, CELL_SIZE*CELL_COUNT+2, CELL_SIZE*CELL_COUNT+2), 5)
+		
+		for body in bodies:
+			body_rect = (OFFSET + body.x * CELL_SIZE, OFFSET+ body.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+			pygame.draw.rect(screen, DARK_BLUE, body_rect, 0, 7)
 
-	screen.blit(title_surface, (OFFSET-15, 2))
-	pygame.display.update()
-	pygame.time.Clock().tick(60)
+		title_surface = pygame.font.Font(None, 15).render("Simple Snake", True, DARK_BLUE)
+
+		screen.blit(title_surface, (OFFSET-15, 2))
+		pygame.display.update()
+		pygame.time.Clock().tick(60)
+
+		await asyncio.sleep(0)
+
+asyncio.run(main())
