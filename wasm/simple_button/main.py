a619a5a50c9b0951a316f01fb2d417aa41b6d37b
jamad <jamad2001@gmail.com>
2023-12-17 21:43:57 +0200

    Update main.py

--- wasm/simple_button/main.py
+++ wasm/simple_button/main.py
@@ -5,17 +5,16 @@ pygame.init()
 w=h=360
 screen = pygame.display.set_mode((h,w) ) 
 smallfont = pygame.font.SysFont('Corbel',35) 
-text = smallfont.render('QUIT' , True , (255,255,255) ) 
 
 async def main(): # step2
 	while True:  
+		text = smallfont.render('QUIT' , True , (255,255,255) ) 
 		x,y = pygame.mouse.get_pos() 
 		hoverover=0 <= x-w/2 <= 140 and 0 <= y-h/2 <= 40
 		
 		for ev in pygame.event.get(): 
 			if ev.type == pygame.QUIT or (ev.type == pygame.MOUSEBUTTONDOWN and hoverover): 
-				pygame.quit() 
-				sys.exit()
+				text = smallfont.render('Clicked Button!' , True , (255,255,255) ) 
 				
 		screen.fill((60,25,60)) 
 		pygame.draw.rect(screen,((100,100,100) ,(170,170,170) )[hoverover],[w/2,h/2,140,40]) 	
