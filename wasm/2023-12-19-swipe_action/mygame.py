f39a541bea498504e1cf9816bb9a7f4c9a4a8fae
jamad <jamad2001@gmail.com>
2023-12-19 18:23:41 +0200

    updated game

--- wasm/2023-12-19-swipe_action/mygame.py
+++ wasm/2023-12-19-swipe_action/mygame.py
@@ -7,16 +7,21 @@ myfont = pygame.font.SysFont(None,25)
 
 # Create a player object
 player = pygame.Surface((50, 50))
-player.fill((255, 0, 0))
 # Set the initial position of the player
 player_pos = [175, 175]
 
+player.fill((255, 0, 0))
+greencolor=0
+
 def mainloop():
-    global player_pos
+    global player_pos, greencolor
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.FINGERDOWN:
             player_pos = event.pos
+            greencolor= not greencolor
+
+    player.fill((255-255*greencolor,255*greencolor,0))
 
     myscreen.fill((0, 0, 0))
     myscreen.blit(player, player_pos)
