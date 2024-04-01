ea1e8d219ad9f1dd15f3c6a5bc3c0689457f8acf
jamad <jamad2001@gmail.com>
2024-04-01 13:47:40 +0300

    Update main.js

--- crisp_game_lib/game001_helloworld/main.js
+++ crisp_game_lib/game001_helloworld/main.js
@@ -6,26 +6,33 @@ characters = [];// 現時点では何に使うか不明
 
 options = {};// 現時点では何に使うか不明
 
-/** @type {Vector[]} */
+/** @type {Vector[]} */ //必須でないが、このsyntaxで型宣言すると、デバッグ等に役立つ
+
 let pins;
+let scroll_y = 0.1234;
+let boxsize = 2.0;
 
 function update() { //1 秒に 60 回呼び出される
     if (!ticks) { //開始フレームのみ実行という意味　つまり初期化処理 
         pins = [vec(50, 5)]; // x=50, y=5
-        nextPinDist = 5; // 現時点では何に使うか不明
+        nextPinDist = 5; // 次に生成するboxのトリガーに利用する変数　負になったら新規生成
     }
 
-    let scroll_y = 1.234;
-    let boxsize = 1.5;
     pins.forEach((pos) => {
-        pos.y += scroll_y;
+        pos.y += scroll_y; // 画面下方向にｙが増加する　右下座標が(99,99)
         box(pos, boxsize);// draw box objects
     });
 
     nextPinDist -= scroll_y;
-    while (nextPinDist < 0) {
-        pins.push(vec(rnd(10, 90), -2 - nextPinDist));
+
+    while (nextPinDist < 0) { //負になったのでboxを新規生成
+        let pos_x = rnd(10, 90);// random value range(10,90)
+        let pos_y = nextPinDist - boxsize; // 必ず画面外に新規boxは描画されるはず
+
+        pins.push(vec(pos_x, pos_y));
+
         nextPinDist += rnd(5, 15);
     }
+
 }
 
