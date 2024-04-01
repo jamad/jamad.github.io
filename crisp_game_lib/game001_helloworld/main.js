3a939fe0f0f3fc68d46ab4959438f91314a0e2ad
jamad <jamad2001@gmail.com>
2024-04-01 14:08:05 +0300

    Update main.js

--- crisp_game_lib/game001_helloworld/main.js
+++ crisp_game_lib/game001_helloworld/main.js
@@ -14,8 +14,8 @@ let boxsize = 2.0;
 
 function update() { //1 秒に 60 回呼び出される
     if (!ticks) { //開始フレームのみ実行という意味　つまり初期化処理 
-        box_list = [vec(50, 5)]; // x=50, y=5
-        nextPinDist = 5; // 次に生成するboxのトリガーに利用する変数　負になったら新規生成
+        box_list = [vec(50, -5)]; // x=50, y=-5　の2Dベクトルを１つ保持
+        nextPinDist = -5; // 次のbox生成トリガーに利用　画面に表示されそうになった時点で次の新規生成を行う
     }
 
     box_list.forEach((pos) => {
@@ -23,15 +23,15 @@ function update() { //1 秒に 60 回呼び出される
         box(pos, boxsize);// draw box objects
     });
 
-    nextPinDist -= scroll_y; // 
+    nextPinDist += scroll_y; //他のbox同様にスクロールさせる 
 
-    while (nextPinDist < 0) { //負になったのでboxを新規生成
+    while (-boxsize * 2 < nextPinDist) { //boxが画面に表示されそうになったので新規生成
         let pos_x = rnd(10, 90);// random value range(10,90)
-        let pos_y = nextPinDist - boxsize; // 必ず画面外に新規boxは描画されるはず
+        let pos_y = nextPinDist; // 必ず画面外に新規boxは描画されるはず
 
         box_list.push(vec(pos_x, pos_y));// box_list にドットを追加
 
-        nextPinDist += rnd(5, 15);// 次のｙの距離までをランダムに決定する
+        nextPinDist = -rnd(5, 15);// 次の生成距離ｙをランダムに決定する
     }
 
 }
