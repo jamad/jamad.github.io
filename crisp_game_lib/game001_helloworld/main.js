963d7ae266204a968ed085ef41431eb513576910
jamad <jamad2001@gmail.com>
2024-04-01 13:22:00 +0300

    Update main.js

--- crisp_game_lib/game001_helloworld/main.js
+++ crisp_game_lib/game001_helloworld/main.js
@@ -9,11 +9,19 @@ options = {};
 /** @type {Vector[]} */
 let pins;
 
-function update() {
-    if (!ticks) {
+function update() { //1 秒に 60 回呼び出される
+    if (!ticks) { //開始フレームのみ実行という意味　つまり初期化処理 
         pins = [vec(50, 5)];
+        nextPinDist = 5;
     }
+    let scr = 0.02;
     pins.forEach((p) => {
+        p.y += scr;
         box(p, 3);
     });
+    nextPinDist -= scr;
+    while (nextPinDist < 0) {
+        pins.push(vec(rnd(10, 90), -2 - nextPinDist));
+        nextPinDist += rnd(5, 15);
+    }
 }
