7949f3666d1ffe3ee9ef5ad1d5d2e0436f5c7056
jamad <jamad2001@gmail.com>
2024-04-02 23:35:16 +0300

    Create main.js

---
+++ crisp_game_lib/game002_abc/main.js
@@ -0,0 +1,20 @@
+title = "ABC";
+
+description = `
+THIS IS
+MULTI LINE
+MULTI LINE
+MULTI LINE
+MULTI LINE
+MULTI LINE
+`;
+
+characters = [];
+
+options = {
+    viewSize: { x: 512, y: 256 },
+    theme: "pixel",
+    isPlayingBgm: true,
+    isReplayEnabled: true,
+    seed: 3000,
+};
