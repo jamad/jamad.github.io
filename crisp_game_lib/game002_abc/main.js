2a0412557d19e5a9a7efd1ef672ab7059bb6ad5d
jamad <jamad2001@gmail.com>
2024-04-04 22:22:18 +0300

    Update main.js

--- crisp_game_lib/game002_abc/main.js
+++ crisp_game_lib/game002_abc/main.js
@@ -19,16 +19,31 @@ options = {
     seed: 3000,
 };
 
-// https://github.com/JunoNgx/crisp-game-lib-tutorial?tab=readme-ov-file#step-013-container-variable-and-jsdoc
 
-/**
-* @typedef {{
-* pos: Vector,
-* speed: number
-* }} Star
-*/
+/** @type {Vector[]} */
+let player = vec(256, 128);
 
-/**
-* @type  { Star [] }
-*/
-let stars;
+let length = 32;
+
+function update() {
+    if (!ticks) {
+        // init
+        angle = 0.0;
+        direction = 1.0;
+        angle_speed = 0.01;
+    }
+
+    box(player, 5);
+
+    if (input.isJustPressed) {
+        direction *= -1;
+        angle_speed = 0.01;
+    }
+
+    angle_speed *= 1.001;
+    angle += angle_speed * direction;
+
+
+    let dest = vec(player).addWithAngle(angle, length);
+    line(player, dest, 1);
+}
